9c477d44de5189dc1b8ad1d849bfecbc32b6f219
Alexis Merino <alexismerino49@gmail.com>
2024-04-21 20:48:12 -0500

    added the sequelize connection and sync to turn on server in  server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+
+// importing express
 const express = require('express');
+// importing the routes from thr routes folder
 const routes = require('./routes');
-// import sequelize connection
+// importing the sequelize connection
+const sequelize = require('./config/connection');
 
+// the constant app is now express
 const app = express();
+// this constant is the port
 const PORT = process.env.PORT || 3001;
-
+// 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
