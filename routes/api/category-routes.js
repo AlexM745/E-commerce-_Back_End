ac05572d84b8a7b06f95fd9fc85eaf091ecd2fdc
Alexis Merino <alexismerino49@gmail.com>
2024-04-23 18:37:57 -0500

    added the associated products to the get calls

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include:[{model: Product,  as: "product_catergory"}]
+      include: {model: Product,  attributes: ["category_id"]}
     });
     if (!categoryData){
       res.status(404).json({message:"No category found with this id!"});
