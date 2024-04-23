b4acbafd79e0f5edcea1b60779ca415036d9a2c0
Alexis Merino <alexismerino49@gmail.com>
2024-04-22 21:04:26 -0500

    added columns to the product tag model with comments

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,34 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    // id column for the product tag table
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // foregin key connection for the products table
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // foregin key connection for the tags table
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
