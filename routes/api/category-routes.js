e5d98b382fa0af7d9f6c7d82e5c19874b64ea56d
Alexis Merino <alexismerino49@gmail.com>
2024-04-23 20:47:51 -0500

    added try catch code to findall, findone, create update and destroy on tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.post('/', async (req, res) => {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 // update a category by its `id` value
